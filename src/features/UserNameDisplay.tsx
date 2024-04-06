ed4696188ebdfc2b75ae00fe24a8b5dc04509b32
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-06 15:26:00 -0500

    feat(User): Add UserNameDisplay component
    Component to display username in the page was added

---
+++ src/features/UserNameDisplay.tsx
@@ -0,0 +1,9 @@
+const UserNameDisplay = ({ name }) => (
+  <div className="mt-40">
+    <h1 className="text-center text-black text-5xl font-bold font-['Inter']">
+      Hi, {name}
+    </h1>
+  </div>
+);
+
+export default UserNameDisplay;
