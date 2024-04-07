8e474b1df433865ba15f2291d20b63cb532acb3c
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-07 00:20:13 -0500

    feat(Tasks): Add task provider
    Task context was added for data persistence

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import '../styles/globals.css'
 
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
+import { TaskProvider } from '../context/TaskContext'
 
 import store from '../app/store'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <TaskProvider>
+        <Component {...pageProps} />
+      </TaskProvider>
     </Provider>
   )
 }
