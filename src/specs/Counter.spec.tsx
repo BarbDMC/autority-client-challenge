5de3e96237cc837bf4c9405e923abf535425322c
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-08 12:01:11 -0500

    test: Move counter spec to specs folder

--- src/specs/Counter.spec.tsx
+++ src/specs/Counter.spec.tsx
@@ -1,16 +1,17 @@
-import { render, screen } from '@testing-library/react'
-import user from '@testing-library/user-event'
 import { Provider } from 'react-redux'
+import user from '@testing-library/user-event'
+import { act } from 'react-dom/test-utils';
+import { render, screen } from '@testing-library/react'
 
-jest.mock('./counterAPI', () => ({
+jest.mock('../features/counter-reference-redux/counterAPI', () => ({
   fetchCount: (amount: number) =>
     new Promise<{ data: number }>((resolve) =>
       setTimeout(() => resolve({ data: amount }), 500)
     ),
 }))
 
-import { makeStore } from '../../app/store'
-import Counter from './Counter'
+import { makeStore } from '../app/store'
+import Counter from '../features/counter-reference-redux/Counter'
 
 describe('<Counter />', () => {
   it('renders the component', () => {
@@ -25,7 +26,7 @@ describe('<Counter />', () => {
     expect(screen.getByText('0')).toBeInTheDocument()
   })
 
-  it('decrements the value', () => {
+  it('decrements the value', async () => {
     const store = makeStore()
 
     render(
@@ -34,12 +35,14 @@ describe('<Counter />', () => {
       </Provider>
     )
 
-    user.click(screen.getByRole('button', { name: /decrement value/i }))
+    await act(async () => {
+      user.click(screen.getByRole('button', { name: /decrement value/i }))
+    });
 
     expect(screen.getByText('-1')).toBeInTheDocument()
   })
 
-  it('increments the value', () => {
+  it('increments the value', async () => {
     const store = makeStore()
 
     render(
@@ -48,12 +51,14 @@ describe('<Counter />', () => {
       </Provider>
     )
 
-    user.click(screen.getByRole('button', { name: /increment value/i }))
+    await act(async () => {
+      user.click(screen.getByRole('button', { name: /increment value/i }))
+    });
 
     expect(screen.getByText('1')).toBeInTheDocument()
   })
 
-  it('increments by amount', () => {
+  it('increments by amount', async () => {
     const store = makeStore()
 
     render(
@@ -62,8 +67,10 @@ describe('<Counter />', () => {
       </Provider>
     )
 
-    user.type(screen.getByLabelText(/set increment amount/i), '{backspace}5')
-    user.click(screen.getByRole('button', { name: /add amount/i }))
+    await act(async () => {
+      user.type(screen.getByLabelText(/set increment amount/i), '{backspace}5')
+      user.click(screen.getByRole('button', { name: /add amount/i }))
+    });
 
     expect(screen.getByText('5')).toBeInTheDocument()
   })
@@ -77,8 +84,10 @@ describe('<Counter />', () => {
       </Provider>
     )
 
-    user.type(screen.getByLabelText(/set increment amount/i), '{backspace}3')
-    user.click(screen.getByRole('button', { name: /add async/i }))
+    await act(async () => {
+      user.type(screen.getByLabelText(/set increment amount/i), '{backspace}3');
+      user.click(screen.getByRole('button', { name: /add async/i }));
+    });
 
     await expect(screen.findByText('3')).resolves.toBeInTheDocument()
   })
@@ -92,13 +101,15 @@ describe('<Counter />', () => {
       </Provider>
     )
 
-    user.click(screen.getByRole('button', { name: /add if odd/i }))
-
-    expect(screen.getByText('0')).toBeInTheDocument()
-
-    user.click(screen.getByRole('button', { name: /increment value/i }))
-    user.type(screen.getByLabelText(/set increment amount/i), '{backspace}8')
-    user.click(screen.getByRole('button', { name: /add if odd/i }))
+    await act(async () => {
+      user.click(screen.getByRole('button', { name: /add if odd/i }))
+  
+      expect(screen.getByText('0')).toBeInTheDocument()
+  
+      user.click(screen.getByRole('button', { name: /increment value/i }))
+      user.type(screen.getByLabelText(/set increment amount/i), '{backspace}8')
+      user.click(screen.getByRole('button', { name: /add if odd/i }))
+    });
 
     await expect(screen.findByText('9')).resolves.toBeInTheDocument()
   })
