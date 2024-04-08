050992d68930fef1702a74be7ec0bedf5a7b5a27
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-08 12:01:57 -0500

    test: Add custom button component spec

---
+++ src/specs/CustomButton.spec.tsx
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import CustomButton from '../features/CustomButton';
+
+describe('CustomButton Component', () => {
+  test('displays provided text', () => {
+    const testText = 'Click Me!';
+    render(<CustomButton text={testText} />);
+    expect(screen.getByRole('button')).toHaveTextContent(testText);
+  });
+
+  test('button is disabled when disabled prop is true', () => {
+    render(<CustomButton disabled={true} />);
+    expect(screen.getByRole('button')).toBeDisabled();
+  });
+
+  test('onClick prop function is called when button is clicked', () => {
+    const handleClick = jest.fn();
+    render(<CustomButton onClick={handleClick} />);
+    fireEvent.click(screen.getByRole('button'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('onClick is not called when button is disabled', () => {
+    const handleClick = jest.fn();
+    render(<CustomButton onClick={handleClick} disabled={true} />);
+    fireEvent.click(screen.getByRole('button'));
+    expect(handleClick).not.toHaveBeenCalled();
+  });
+});
