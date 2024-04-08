d0c1dc439cc3b20157661c1d99e3e33b5728fdd6
BarbDMC <barbaradmorantesc@gmail.com>
2024-04-08 12:52:16 -0500

    chore(config): Add initial .env file with base API URL

--- src/config/axiosInstance.ts
+++ src/config/axiosInstance.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: apiBaseUrl,
   timeout: 10000
 });
